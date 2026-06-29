62c995ef46303d7759392d1f38a27da331602b9e
ashwinmanian2005-web <ashwinmanian2005@gmail.com>
2026-06-29 09:09:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/ashwin-portfolio/',
+    base:'/aswin-portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
